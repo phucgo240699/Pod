f39eac9afb771d7fb8abab51aeacea199593b80c
phuc <phucgo240699@gmail.com>
2021-05-14 21:40:40 +0700

    fix performance

--- Pod/Animation.cpp
+++ Pod/Animation.cpp
@@ -107,5 +107,5 @@ void Animation::Update(float _dt)
 
 void Animation::Draw(LPDIRECT3DTEXTURE9 _texture, D3DXVECTOR3* _position, D3DCOLOR _color, bool _isFlip)
 {
-	Drawing::getInstance()->draw(_texture, this->getCurrentFrame(), NULL, _position, _color, _isFlip, new D3DXVECTOR2(0, -this->getCurrentFrameHeight()));
+	Drawing::getInstance()->draw(_texture, this->getCurrentFrame(), NULL, _position, D3DXVECTOR2(0, -this->getCurrentFrameHeight()), _color, _isFlip);
 }
--- Pod/Drawing.cpp
+++ Pod/Drawing.cpp
@@ -9,24 +9,24 @@ Drawing* Drawing::getInstance()
 	return instance;
 }
 
-void Drawing::draw(LPDIRECT3DTEXTURE9 texture, RECT* _srcRect, D3DXVECTOR3* _center, D3DXVECTOR3* _position, D3DCOLOR _color, bool _isFlip, D3DXVECTOR2* _translation)
+void Drawing::draw(LPDIRECT3DTEXTURE9 texture, RECT* _srcRect, D3DXVECTOR3* _center, D3DXVECTOR3* _position, D3DXVECTOR2 _translation, D3DCOLOR _color, bool _isFlip)
 {
-	D3DXVECTOR2* scale;
-	D3DXVECTOR2* scalePoint;
+	D3DXVECTOR2 scale;
+	D3DXVECTOR2 scalePoint;
 	D3DXMATRIX matrix;
 	D3DXMATRIX oldMatrix;
 	if (_isFlip) {
 		float width = _srcRect->right - _srcRect->left;
 		float height = _srcRect->bottom - _srcRect->top;
-		scale = new D3DXVECTOR2(-1, 1);
-		scalePoint = new D3DXVECTOR2(_position->x + width / 2, _position->y + height / 2);
+		scale = D3DXVECTOR2(-1, 1);
+		scalePoint = D3DXVECTOR2(_position->x + width / 2, _position->y + height / 2);
 	}
 	else {
-		scale = new D3DXVECTOR2(1, 1);
-		scalePoint = new D3DXVECTOR2(0, 0);
+		scale = D3DXVECTOR2(1, 1);
+		scalePoint = D3DXVECTOR2(0, 0);
 	}
 
-	D3DXMatrixTransformation2D(&matrix, scalePoint, 0, scale, new D3DXVECTOR2(0, 0), 0, _translation);
+	D3DXMatrixTransformation2D(&matrix, &scalePoint, 0, &scale, new D3DXVECTOR2(0, 0), 0, &_translation);
 
 	spriteHandler->GetTransform(&oldMatrix);
 	spriteHandler->SetTransform(&matrix);
@@ -36,6 +36,11 @@ void Drawing::draw(LPDIRECT3DTEXTURE9 texture, RECT* _srcRect, D3DXVECTOR3* _cen
 	/*else {
 		spriteHandler->Draw(texture, _srcRect, _center, _position, _color);
 	}*/
+}
+
+void Drawing::draw(LPDIRECT3DTEXTURE9 texture, RECT* _srcRect, D3DXVECTOR3* _center, D3DXVECTOR3* _position, D3DCOLOR _color, bool _isFlip)
+{
+	spriteHandler->Draw(texture, _srcRect, _center, _position, _color);	
 }
 
 //void Drawing::draw(LPDIRECT3DSURFACE9 surface, RECT* _srcRect, D3DCOLOR _color)
