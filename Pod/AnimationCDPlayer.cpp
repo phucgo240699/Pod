5929992aa2b7af245e6fd97b9d83a2809c324191
phucgo240699 <phucgo240699@gmail.com>
2021-07-19 08:39:25 +0700

    fix animation cd player

--- Pod/AnimationCDPlayer.cpp
+++ Pod/AnimationCDPlayer.cpp
@@ -10,25 +10,35 @@ AnimationCDPlayer* AnimationCDPlayer::getInstance()
 
 void AnimationCDPlayer::Update(float _dt)
 {
-	for (auto cd = listCD.begin(); cd != listCD.end();) {
-		(*cd).second->Update(_dt);
 
-		//// Check is CD finish
-		//if ((*cd).second->getIsFinish()) {
-		//	this->listCD.erase(cd++);
+	//for (auto cd = listCD.begin(); cd != listCD.end();) {
+	//	// Check is CD finish
+	//	if ((*cd).second->getIsFinish()) {
+	//		this->listCD.erase(cd++);
+	//	}
+	//	else {
+	//		++cd;
+	//	}
+	//}
+
+	for (auto cd = listCD.begin(); cd != listCD.end();) {
+		//if ((*cd).second->getIsFinish() == false) {
+			if ((*cd).second->getIsFinish()) {
+				this->listCD.erase(cd++);
+			}
+			else {
+				(*cd).second->Update(_dt);
+				++cd;
+			}
+			// Check is CD finish
 		//}
-		//else {
-		//	++cd;
-		//}
-		auto currentCd = cd++;
-		if ((*currentCd).second->getIsFinish()) {
-			listCD.erase(currentCd);
-		}
 	}
+
 }
 
 void AnimationCDPlayer::Draw(LPDIRECT3DTEXTURE9 _texture)
 {
+
 	for (auto cd = listCD.begin(); cd != listCD.end(); cd++) {
 		(*cd).second->Draw(_texture);
 	}
